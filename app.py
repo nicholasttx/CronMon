721e6f83912ae9345d0cf4986521529f977325e0
willie <willie@willietest.com>
2020-01-13 09:12:04 -0600

    remove while

--- app.py
+++ app.py
@@ -28,31 +28,29 @@ headers = {"Authorization": "Bearer {0}".format(myToken)}
 # list for storing podsData
 podsDataList = []
 
-# adding while loop to avoid openshift 'crashloopback' pod error
-while 1:
 
 #####################################################################################
-    # loop the list to get pods in each namespace
-    for ns in namespaces:
-       apiUrl = myUrl + "/api/v1/namespaces/{0}/pods".format(ns)
-       podsData = requests.get(apiUrl, headers=headers,verify=False).json()
-       podsDataList.append(podsData)
+# loop the list to get pods in each namespace
+for ns in namespaces:
+    apiUrl = myUrl + "/api/v1/namespaces/{0}/pods".format(ns)
+    podsData = requests.get(apiUrl, headers=headers,verify=False).json()
+    podsDataList.append(podsData)
 
 
-    # looping podsDataList to get all pods states
-    for podData in podsDataList:
-        for item in podData["items"]:
-            # check if pod is at 'Running' state
-            # if not, the report problems:
-            if item["status"]["phase"].lower() != "running":
-                # writing data in to a file
-                with open('resuilt.txt', 'a') as f:
-                    f.write("POD Error! --  {0}/{1} is at <{2}>\n".format(item["metadata"]["namespace"],item["metadata"]["name"], item["status"]["phase"]))
+# looping podsDataList to get all pods states
+for podData in podsDataList:
+    for item in podData["items"]:
+        # check if pod is at 'Running' state
+        # if not, the report problems:
+        if item["status"]["phase"].lower() != "running":
+            # writing data in to a file
+            with open('result.txt', 'a') as f:
+                f.write("POD Error! --  {0}/{1} is at <{2}>\n".format(item["metadata"]["namespace"],item["metadata"]["name"], item["status"]["phase"]))
 #                f.write("Pod Name: {0}/{1} -- Pod State: {2} \n".format(item["metadata"]["namespace"],item["metadata"]["name"], item["status"]["phase"]))
 
 
 
 ####################################################################################
 
-    # sleep the program
-    time.sleep(60) 
+# sleep the program
+time.sleep(20) 
